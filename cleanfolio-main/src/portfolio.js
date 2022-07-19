de49f3cf42ba6446aa3b76ce3281c0ded585d489
K-u-s-h-gupta <97623935+K-u-s-h-gupta@users.noreply.github.com>
2022-07-19 10:30:53 +0530

    Portfolio

---
+++ cleanfolio-main/src/portfolio.js
@@ -0,0 +1,61 @@
+const header = {
+  homepage: '#',
+  title: 'KUSH.',
+}
+
+const about = {
+  name: 'Kushagra Gupta',
+  role: 'Front End Engineer',
+  description:
+    'Courteous and enthusiastic, I am interested in IT and everything in its orbit. I recently began to be fascinated by web programming, e.g. developing apps and building websites. I am keen to gain experience in this field. For this reason, I am currently looking for a job or an internship as a front-end developer.',
+  resume:
+    'https://drive.google.com/file/d/1d_Ym_p6oAt-lgKS4bVZHbFrf2qKp6mOV/view?usp=sharing',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/kushagra-gupta-3bb00a232',
+    github: 'https://github.com/K-u-s-h-gupta',
+  },
+}
+
+const projects = [
+  {
+    name: 'IP Address Tracker',
+    description:
+      'In this you can trace the location, ISP, Timezone and even location on the map with marker',
+    stack: ['React-Leaflet', 'React'],
+    sourceCode: 'https://github.com/K-u-s-h-gupta/Ip_address_tracker_Reactjs',
+    livePreview: 'https://enchanting-tartufo-697eca.netlify.app/',
+  },
+  {
+    name: 'Speed Typing Game',
+    description:
+      'In this app you can test your speed how much fast you can type with ineractive user interface',
+    stack: ['React'],
+    sourceCode: 'https://github.com/K-u-s-h-gupta/speed-typing-react',
+    livePreview: 'https://gleaming-profiterole-ed7c80.netlify.app/',
+  },
+  {
+    name: 'Countries API',
+    description:
+      'In this you can search for any country , filter countries by region , click on the country to see more details',
+    stack: ['React-Router', 'React'],
+    sourceCode: 'https://github.com/K-u-s-h-gupta/React-countriesApi',
+    livePreview: 'https://candid-granita-6a2ad3.netlify.app/',
+  },
+]
+
+const skills = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'Bootstrap',
+  'React',
+  'Material UI',
+  'Git',
+  'C',
+]
+
+const contact = {
+  email: 'kushagr18gupta@gmail.com',
+}
+
+export { header, about, projects, skills, contact }
